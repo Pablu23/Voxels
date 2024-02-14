cca6c69652cf6d9bcf1ff2bf5249569a4c0a2986
Pablu23 <pablu23@gmx.de>
2024-02-14 17:26:02 +0100

    Texture

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Xml;
-using Silk.NET.Input;
+﻿using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using StbImageSharp;
+using Color = System.Drawing.Color;
+using Window = Silk.NET.Windowing.Window;
 
 namespace Voxels;
 
@@ -13,43 +15,39 @@ public static class Program
 
     private static uint _vbo, _ebo, _vao, _shader;
 
+    private static uint _texture;
+
     private const string VertexShaderSource = """
                                               #version 330 core
-                                              layout (location=0) in vec4 vPos;
+                                              layout (location=0) in vec3 aPos;
 
+                                              layout (location = 1) in vec2 aTexCoords;
+                                              
+                                              out vec2 frag_texCoords;
+                                              
                                               void main()
                                               {
-                                                  gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
+                                                  gl_Position = vec4(aPos, 1.0);
+                                                  
+                                                  frag_texCoords = aTexCoords;
                                               }
                                               """;
 
     private const string FragmentShaderSource = """
                                                 #version 330 core
-                                                out vec4 FragColor;
+                                                in vec2 frag_texCoords;
+
+                                                out vec4 out_color;
+                                                
+                                                uniform sampler2D uTexture;
                                                 
                                                 void main()
                                                 {
-                                                    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                                                    out_color = texture(uTexture, frag_texCoords);
                                                 }
                                                 """;
-    
-    //Vertex data, uploaded to the VBO.
-    private static readonly float[] Vertices =
-    [
-        //X    Y      Z
-        0.5f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.5f
-    ];
 
-    //Index data, uploaded to the EBO.
-    private static readonly uint[] Indices =
-    [
-        0, 1, 3,
-        1, 2, 3
-    ];
-    
+
     public static void Main()
     {
         var options = WindowOptions.Default;
@@ -62,74 +60,133 @@ public static class Program
         _window.Update += OnUpdate;
         _window.Render += OnRender;
         _window.Closing += OnClose;
+        _window.Resize += OnResize;
         
         _window.Run();
         _window.Dispose();
     }
 
-    
 
     private static unsafe void OnLoad()
     {
         //Set-up input context.
         IInputContext input = _window.CreateInput();
-        for (int i = 0; i < input.Keyboards.Count; i++)
+        foreach (IKeyboard key in input.Keyboards)
         {
-            input.Keyboards[i].KeyDown += KeyDown;
+            key.KeyDown += KeyDown;
         }
 
         _gl = GL.GetApi(_window);
+
+        _gl.ClearColor(Color.CornflowerBlue);
+
         _vao = _gl.GenVertexArray();
         _gl.BindVertexArray(_vao);
 
+        //Vertex data, uploaded to the VBO.
+        float[] vertices =
+        [
+            //X    Y      Z
+            0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+            -0.5f, 0.5f, 0.5f, 0.0f, 1.0f
+        ];
+        
         _vbo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
-        fixed (void* v = &Vertices[0])
+        fixed (float* buf = vertices)
         {
-            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (Vertices.Length * sizeof(uint)), v, BufferUsageARB.StaticDraw);
+            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(uint)), buf,
+                BufferUsageARB.StaticDraw);
         }
 
+        //Index data, uploaded to the EBO.
+        uint[] indices =
+        [
+            0, 1, 3,
+            1, 2, 3
+        ];
+        
         _ebo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
-        fixed (void* i = &Indices[0])
+        fixed (uint* buf = indices)
         {
-            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (Indices.Length * sizeof(uint)), i, BufferUsageARB.StaticDraw);
+            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(indices.Length * sizeof(uint)), buf,
+                BufferUsageARB.StaticDraw);
         }
 
         uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
         _gl.ShaderSource(vertexShader, VertexShaderSource);
         _gl.CompileShader(vertexShader);
 
-        string infoLog = _gl.GetShaderInfoLog(vertexShader);
-        if (!string.IsNullOrWhiteSpace(infoLog))
-            Console.WriteLine($"Error compiling vertex Shader {infoLog}");
+        _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+        if (vStatus != (int) GLEnum.True)
+            throw new Exception($"Vertex Shader failed to compile: {_gl.GetShaderInfoLog(vertexShader)}");
 
         uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
         _gl.ShaderSource(fragmentShader, FragmentShaderSource);
         _gl.CompileShader(fragmentShader);
-        
-        infoLog = _gl.GetShaderInfoLog(fragmentShader);
-        if (!string.IsNullOrWhiteSpace(infoLog))
-            Console.WriteLine($"Error compiling fragment Shader {infoLog}");
+
+        _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
+        if (fStatus != (int) GLEnum.True)
+            throw new Exception($"Fragment Shader failed to compile: {_gl.GetShaderInfoLog(fragmentShader)}");
 
         _shader = _gl.CreateProgram();
+        
         _gl.AttachShader(_shader, vertexShader);
         _gl.AttachShader(_shader, fragmentShader);
         _gl.LinkProgram(_shader);
 
-        _gl.GetProgram(_shader, GLEnum.LinkStatus, out int status);
-        if (status == 0)
-        {
-            Console.WriteLine($"Error linking shaders {_gl.GetProgramInfoLog(_shader)}");
-        }
+        _gl.GetProgram(_shader, GLEnum.LinkStatus, out int lStatus);
+        if (lStatus != (int) GLEnum.True)
+            throw new Exception($"Program failed to link: {_gl.GetProgramInfoLog(_shader)}");
         
         _gl.DetachShader(_shader, vertexShader);
         _gl.DetachShader(_shader, fragmentShader);
         _gl.DeleteShader(vertexShader);
         _gl.DeleteShader(fragmentShader);
+
+        const uint stride = 3 * sizeof(float) + 2 * sizeof(float);
+
+        const uint positionLoc = 0;
+        _gl.EnableVertexAttribArray(positionLoc);
+        _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, stride, null);
+
+        const uint textureLoc = 1;
+        _gl.EnableVertexAttribArray(textureLoc);
+        _gl.VertexAttribPointer(textureLoc, 2, VertexAttribPointerType.Float, false, stride, (void*) (3 * sizeof(float)));
         
-        _gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
-        _gl.EnableVertexAttribArray(0);
+        _gl.BindVertexArray(0);
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+
+        _texture = _gl.GenTexture();
+        _gl.ActiveTexture(TextureUnit.Texture0);
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+
+        ImageResult result = ImageResult.FromMemory(File.ReadAllBytes("silk.png"), ColorComponents.RedGreenBlueAlpha);
+
+        fixed (byte* ptr = result.Data)
+        {
+            _gl.TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint) result.Width, (uint) result.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr);
+        }
+        
+        _gl.TextureParameter(_texture, TextureParameterName.TextureWrapS, (int) TextureWrapMode.Repeat);
+        _gl.TextureParameter(_texture, TextureParameterName.TextureWrapT, (int) TextureWrapMode.Repeat);
+
+        _gl.TextureParameter(_texture, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+        _gl.TextureParameter(_texture, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Linear);
+        
+        _gl.GenerateMipmap(TextureTarget.Texture2D);
+        
+        _gl.BindTexture(TextureTarget.Texture2D, 0);
+
+        int location = _gl.GetUniformLocation(_shader, "uTexture");
+        _gl.Uniform1(location, 0);
+        
+        _gl.Enable(EnableCap.Blend);
+        _gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusConstantAlpha);
     }
 
     private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
@@ -142,15 +199,24 @@ public static class Program
 
     private static void OnUpdate(double obj)
     {
-        
     }
 
     private static unsafe void OnRender(double obj)
     {
-        _gl.Clear((uint) ClearBufferMask.ColorBufferBit);
+        _gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+        
         _gl.BindVertexArray(_vao);
         _gl.UseProgram(_shader);
-        _gl.DrawElements(PrimitiveType.Triangles, (uint) Indices.Length, DrawElementsType.UnsignedInt, null);
+        
+        _gl.ActiveTexture(TextureUnit.Texture0);
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+        
+        _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, null);
+    }
+
+    private static void OnResize(Vector2D<int> size)
+    {
+        _gl.Viewport(0, 0, (uint) size.X, (uint) size.Y);
     }
     
     private static void OnClose()
